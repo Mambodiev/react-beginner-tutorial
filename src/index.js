59cccb6f7ec1e82bf64d0d1c91d5254d57117d0d
mambodieu <mambodieu@gmail.com>
2022-02-28 16:13:57 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+function MyfirstFunction(){
+  return (
+    <div>
+      <img src='./logo.svg' width='40px' />
+      <h1 className="main--title">Fun facts about React</h1>
+      <ol className="main--facts">
+          <li>I beleave recat is the most popular</li>
+          <li>I'm sure i'll know it</li>
+          <li>I'm more likely to get a job</li>
+          <li>I'm sure i'll know it</li>
+          
+      </ol>
+    </div>
+  )
+}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <MyfirstFunction />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
