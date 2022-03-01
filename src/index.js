3d4fa7861becdd96865f040d388a91c668c0de0d
mambodieu <mambodieu@gmail.com>
2022-02-28 21:16:34 -0400

    styling with classes

--- src/index.js
+++ src/index.js
@@ -1,40 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './style.css';
+import logo from './logo.svg';
+
 
 function MyHeader(){
   return (
     <header>
-      <nav>
-      <img src='./logo.svg' width='40px' />
-
+      <nav className='nav'>
+        <img src={logo}  alt="logo" className='nav-logo' />
+        <ul className='nav-items' >
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
       </nav>
     </header>
   )
 }
 
-function MyFooter(){
-  return (
-    <footer>
-      <small>@ 2022 Diev development, All rights reserved.</small>
-    </footer>
-  )
-}
+
 
 function MainContent(){
   return (
     <div>
-      <h1 className="main--title">Fun facts about React</h1>
-      <ol className="main--facts">
+      <h1>Fun facts about React</h1>
+      <ol>
           <li>I beleave recat is the most popular</li>
           <li>I'm sure i'll know it</li>
           <li>I'm more likely to get a job</li>
-          <li>I'm sure i'll know it</li>
           
       </ol>
     </div>
   )
 }
 
+function MyFooter(){
+  return (
+    <footer >
+      <small>@ 2022 Diev development, All rights reserved.</small>
+    </footer>
+  )
+}
 
 function Page(){
   return(
